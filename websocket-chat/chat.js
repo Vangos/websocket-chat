b6dd6efae88241a21e8e26f5e28e274241824aa0
Vangos <pterneas@gmail.com>
2014-12-26 15:58:48 +0200

    Initial commit

---
+++ websocket-chat/chat.js
@@ -0,0 +1,88 @@
+/**
+* Executed when the page has finished loading.
+*/
+window.onload = function () {
+
+    // Create a reference for the required DOM elements.
+    var textView = document.getElementById("text-view");
+    var buttonSend = document.getElementById("send-button");
+    var buttonStop = document.getElementById("stop-button");
+    var label = document.getElementById("status-label");
+
+    // Connect to the WebSocket server!
+    var socket = new WebSocket("ws://echo.websocket.org");
+
+    /**
+    * WebSocket onopen event.
+    */
+    socket.onopen = function (event) {
+        label.innerHTML = "Connection open";
+    }
+
+    /**
+    * WebSocket onmessage event.
+    */
+    socket.onmessage = function (event) {
+        if (typeof event.data === "string") {
+            // Display message.
+            label.innerHTML = label.innerHTML + "<br />Server says: " + event.data;
+        }
+    }
+
+    /**
+    * WebSocket onclose event.
+    */
+    socket.onclose = function (event) {
+        var code = event.code;
+        var reason = event.reason;
+        var wasClean = event.wasClean;
+
+        if (wasClean) {
+            label.innerHTML = "Connection closed normally.";
+        }
+        else {
+            label.innerHTML = "Connection closed with message: " + reason + " (Code: " + code + ")";
+        }
+    }
+
+    /**
+    * WebSocket onerror event.
+    */
+    socket.onerror = function (event) {
+        label.innerHTML = "Error: " + event;
+    }
+
+    /**
+    * Disconnect and close the connection.
+    */
+    buttonStop.onclick = function (event) {
+        if (socket.readyState == WebSocket.OPEN) {
+            socket.close();
+        }
+    }
+
+    /**
+    * Send the message and empty the text field.
+    */
+    buttonSend.onclick = function (event) {
+        sendMessage();
+    }
+
+    /**
+    * Send the message and empty the text field.
+    */
+    textView.onkeypress = function (event) {
+        if (event.keyCode == 13) {
+            sendMessage();
+        }
+    }
+
+    function sendMessage() {
+        if (socket.readyState == WebSocket.OPEN) {
+            socket.send(textView.value);
+
+            textView.value = "";
+            label.innerHTML = label.innerHTML + "<br />You say: " + textView.value;
+        }
+    }
+}
